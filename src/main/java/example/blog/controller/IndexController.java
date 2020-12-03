be3369dc2c04efbbad35bdbeaa23b8f3b3545440
lhapak <lukaszrdw@gmail.com>
2020-12-03 15:46:22 +0100

    index controller

---
+++ src/main/java/example/blog/controller/IndexController.java
@@ -0,0 +1,14 @@
+package example.blog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+	
+	@GetMapping("/")
+	String index() {
+		return "index";
+	}
+
+}
