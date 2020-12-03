bd51791f9c8d4a34a99175aab22596573f912bfc
lhapak <lukaszrdw@gmail.com>
2020-12-03 16:25:15 +0100

    User entity

---
+++ src/main/java/example/blog/entity/User.java
@@ -0,0 +1,22 @@
+package example.blog.entity;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String userName;
+	private String password;
+	private String email;
+
+}
