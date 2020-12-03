ed7d0b915403997c2510ae320cab2bcd81422d28
lhapak <lukaszrdw@gmail.com>
2020-12-03 16:11:50 +0100

    displaying posts on index page

--- src/main/java/example/blog/controller/IndexController.java
+++ src/main/java/example/blog/controller/IndexController.java
@@ -1,13 +1,22 @@
 package example.blog.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import example.blog.entity.Post;
+import example.blog.service.PostService;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 	
+	private final PostService postService;
+	
 	@GetMapping("/")
-	String index() {
+	String index(Model model) {
+		model.addAttribute("posts", postService.findAll());
 		return "index";
 	}
 
