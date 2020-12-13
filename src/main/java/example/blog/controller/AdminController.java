a351feb7535819420d9762db28d4a585cb5cf350
lhapak <lukaszrdw@gmail.com>
2020-12-13 13:45:49 +0100

    add post form

--- src/main/java/example/blog/controller/AdminController.java
+++ src/main/java/example/blog/controller/AdminController.java
@@ -1,14 +1,19 @@
 package example.blog.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import example.blog.entity.Post;
 
 @Controller
 public class AdminController {
 
 	
 	@GetMapping("/admin")
-	public String admin() {
+	public String admin(Model model) {
+		Post post = new Post();
+		model.addAttribute("post", post);
 		return"admin/admin";
 	}
 }
