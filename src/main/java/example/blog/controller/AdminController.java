4280eae887d1215c72e49e0e7314abfd31eeff7f
lhapak <lukaszrdw@gmail.com>
2020-12-13 21:39:26 +0100

    persisting posts in database

--- src/main/java/example/blog/controller/AdminController.java
+++ src/main/java/example/blog/controller/AdminController.java
@@ -3,17 +3,28 @@ package example.blog.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import example.blog.entity.Post;
+import example.blog.service.PostService;
+import lombok.RequiredArgsConstructor;
 
 @Controller
+@RequiredArgsConstructor
 public class AdminController {
 
+	final private PostService postService;
 	
 	@GetMapping("/admin")
 	public String admin(Model model) {
 		Post post = new Post();
 		model.addAttribute("post", post);
 		return"admin/admin";
+	}
+	
+	@PostMapping("/post/add")
+	public String addPost(Post post) {
+		postService.save(post);
+		return "redirect:/";
 	}
 }
