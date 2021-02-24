79bf9f1bce6fbee3077b1982f283017b4e4b5921
lhapak <lukaszrdw@gmail.com>
2021-02-24 14:19:45 +0100

    register validation

--- src/main/java/example/blog/controller/UserController.java
+++ src/main/java/example/blog/controller/UserController.java
@@ -5,8 +5,11 @@ import example.blog.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequiredArgsConstructor
@@ -21,7 +24,13 @@ public class UserController {
     }
 
     @PostMapping("/register")
-    public String register(User user) {
+    public String register(@Valid User user, BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            System.out.println("error");
+            return "user/register";
+        }
+        System.out.println("no error");
         user.setRole(example.blog.enums.Role.ROLE_USER);
         userService.save(user);
         return "redirect:/";
--- src/main/java/example/blog/entity/User.java
+++ src/main/java/example/blog/entity/User.java
@@ -1,10 +1,11 @@
 package example.blog.entity;
 
-
 import example.blog.enums.Role;
 import lombok.Data;
 
 import javax.persistence.*;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @Entity
@@ -14,8 +15,12 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Size(max = 24, message = "Nazwa użytkownika nie może być dłuższa niż 24 znaki")
+    @Size(min = 3, message = "Nazwa użytkownika musi się zkładać z conajmniej 3 znaków")
     private String userName;
+    @Size(min = 8, message = "Hasło musi się składać z conajmniej 8 znaków")
     private String password;
+    @Pattern(regexp = "^(.+)@(.+)$", message = "Podaj prawidłowy adres email")
     private String email;
     private Role role;
     @OneToMany(mappedBy = "author", cascade = CascadeType.REMOVE)
