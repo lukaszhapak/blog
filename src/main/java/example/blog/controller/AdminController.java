e8b6c0416901f27392862cece45b3d4ef2a78a38
lhapak <lukaszrdw@gmail.com>
2021-02-25 10:21:49 +0100

    post validation

--- src/main/java/example/blog/controller/AdminController.java
+++ src/main/java/example/blog/controller/AdminController.java
@@ -5,9 +5,12 @@ import example.blog.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequiredArgsConstructor
@@ -23,7 +26,10 @@ public class AdminController {
     }
 
     @PostMapping("/admin/post/add")
-    public String addPost(Post post) {
+    public String addPost(@Valid Post post, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "admin/admin";
+        }
         postService.save(post);
         return "redirect:/";
     }
--- src/main/java/example/blog/entity/Post.java
+++ src/main/java/example/blog/entity/Post.java
@@ -3,6 +3,7 @@ package example.blog.entity;
 import lombok.Data;
 
 import javax.persistence.*;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @Data
@@ -12,7 +13,11 @@ public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Size(max = 64, message = "Tytuł posta nie może być dłuższy niż 64 znaki")
+    @Size(min = 3, message = "Tytuł musi się składać z conajmniej 3 znaków")
     private String title;
+    @Size(max = 1000, message = "Tekst nie może przekraczać 1000 znaków")
+    @Size(min = 5, message = "Tekst musi zawierać przynajmniej 5 znaków")
     @Column(columnDefinition = "TEXT")
     private String text;
     @OneToMany(mappedBy = "post", cascade = CascadeType.REMOVE)
